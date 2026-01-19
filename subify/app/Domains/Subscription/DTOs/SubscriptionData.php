38f5c20b3f700d789192a9ebab649c64f3d9fd4b
web-inwall <52354724+web-inwall@users.noreply.github.com>
2026-01-19 22:53:34 +0300

    feat: add SubscriptionData DTO with validation

---
+++ subify/app/Domains/Subscription/DTOs/SubscriptionData.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domains\Subscription\DTOs;
+
+use Spatie\LaravelData\Attributes\Validation\IntegerType;
+use Spatie\LaravelData\Attributes\Validation\Required;
+use Spatie\LaravelData\Attributes\Validation\StringType;
+use Spatie\LaravelData\Data;
+
+readonly class SubscriptionData extends Data
+{
+    public function __construct(
+        #[Required, IntegerType]
+        public int $userId,
+
+        #[Required, StringType]
+        public string $planKey,
+
+        #[Required, StringType]
+        public string $paymentMethodId,
+    ) {}
+}
